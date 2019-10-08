1ebcd00c4a94704291227e25c434c3644c5c72e0
1250148790 <1250148790@qq.com>
2019-10-08 17:28:54 +0800

    creat profile

---
+++ src/pages/index/profile/profile.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class profile extends Component {
+    render() {
+        return (
+            <div>
+                profile
+            </div>
+        );
+    }
+}
+
+export default profile;
