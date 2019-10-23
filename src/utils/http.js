1fe83db7e5aee667035a9709075084201cd3a9c0
gs95 <745058749@qq.com>
2019-10-23 21:00:34 +0800

    http封装

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,18 @@
 //请求数据
 export default {
-  get: ({url}) => {
-    return fetch(url)
+  get: (url,options) => {
+    return fetch(url,options||{})
+      .then(response => response.json())
+      .then(result => {
+        return result
+      })
+  },
+  post: (url,options) => {
+    options=options||{}
+    return fetch(url,{
+      method:'POST',
+      ...options
+    })
       .then(response => response.json())
       .then(result => {
         return result
