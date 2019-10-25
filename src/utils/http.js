fafaa38e2e9176e89925ecf69b1abd751859f0f7
gs95 <745058749@qq.com>
2019-10-25 21:12:30 +0800

    http 修复

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,38 +1,28 @@
 //请求数据
 export default {
-  get: (url, options) => {
-    console.log(options)
-    console.log(url)
-    return fetch(url)
+  get: (url,options) => {
+    return fetch(url,options||{})
       .then(response => {
-        localStorage.setItem("x-access-token",response.headers.get('content-type'))
         return response.json()
-      }
-      )
+      })
       .then(result => {
-        console.log(result)
         return result
       })
   },
-  post: (url, options) => {
-    options = options || {}
-    if (typeof options.body === 'object') {
-      options.body = JSON.stringify(options.body)
+  post: (url,options) => {
+    options=options||{}
+    if(typeof options.body ==='object'){
+      options.body=JSON.stringify(options.body)
     }
-    if (typeof options.data === 'object') {
-      options.body = JSON.stringify(options.data)
+    if(typeof options.data ==='object'){
+      options.body=JSON.stringify(options.data)
     }
-    return fetch(url, {
-      method: 'POST',
+    return fetch(url,{
+      method:'POST',
       ...options
     })
-      .then(response => {
-        localStorage.setItem("x-access-token",response.headers.get('content-type'))
-        return response.json()
-      }
-      )
+      .then(response => response.json())
       .then(result => {
-        Headers.values()
         return result
       })
   }
